092f39d3bd9806f16a2525c07893bacc02395119
Aaron Guo <qga6@sfu.ca>
2019-07-15 00:21:29 -0700

    added horizongtal win check, didn't work

--- Connect4_test.cc
+++ Connect4_test.cc
@@ -8,24 +8,29 @@ int main () {
 	game.Print_Game_Board();
 
 	//fill the second col on the game board
-	std:: cout << game.Make_a_Move (1) << std:: endl;
+	std:: cout << game.Make_a_Move (3) << std:: endl;
+	game.Print_Game_Board();
+	std:: cout << game.Make_a_Move (4) << std:: endl;
+	game.Print_Game_Board();
+	std:: cout << game.Make_a_Move (2) << std:: endl;
+	game.Print_Game_Board();
+	std:: cout << game.Make_a_Move (4) << std:: endl;
 	game.Print_Game_Board();
 	std:: cout << game.Make_a_Move (1) << std:: endl;
 	game.Print_Game_Board();
-	std:: cout << game.Make_a_Move (1) << std:: endl;
+	std:: cout << game.Make_a_Move (4) << std:: endl;
 	game.Print_Game_Board();
-	std:: cout << game.Make_a_Move (1) << std:: endl;
+	std:: cout << game.Make_a_Move (0) << std:: endl;
 	game.Print_Game_Board();
-	std:: cout << game.Make_a_Move (1) << std:: endl;
-	game.Print_Game_Board();
-	std:: cout << game.Make_a_Move (1) << std:: endl;
-	game.Print_Game_Board();
-	
+	std::cout << "Done\n";
 
 	//this one should fail.
 	std:: cout << game.Make_a_Move (1) << std:: endl;
 
 	std::cout << "Total move: " << +game.Get_Total_Move() << std::endl;
+
+
+
 
 	return 0;
 }
